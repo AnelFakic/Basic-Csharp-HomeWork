26e72ef1f58c281327f37f999c610a1e8404c0b0
Anel Fakic <149324618+AnelFakic@users.noreply.github.com>
2024-03-01 20:49:31 +0100

    Homework #1 Stickman

---
+++ Anel Fakic Homework #1 stick man/Anel Fakic Homework #1 stick man/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Anel_Fakic_Homework__1_stick_man
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  ***");
+            Console.WriteLine("  ***");
+            Console.WriteLine("   *");
+            Console.WriteLine("*******");
+            Console.WriteLine("*  *  *");
+            Console.WriteLine("*  *  *");
+            Console.WriteLine("   *");
+            Console.WriteLine("   *");
+            Console.WriteLine("*******");
+            Console.WriteLine("*     *");
+            Console.WriteLine("*     *");
+            Console.WriteLine("*     *");
+        }
+    }
+}
