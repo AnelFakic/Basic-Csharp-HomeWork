e012d7b887054b97df721e253d10d289ad2af1ee
Anel Fakic <149324618+AnelFakic@users.noreply.github.com>
2024-03-06 16:00:07 +0100

    Added Homework #4 unfinished only Task1 & Task2

---
+++ Homework Class 4/Homework Class 4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework_Class_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // array[6]
+            int[] emptySumArry= new int[6];
+            int sum = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Enter number:");
+                emptySumArry[i]=int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0;i < emptySumArry.Length; i++)
+            {
+                sum = sum + emptySumArry[i];
+                
+            }
+            Console.WriteLine($"The sum of all array elemts is {sum}");
+        }
+    }
+}
---
+++ Homework Class 4/Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] g1Students = {"Marko","Janko","Stanko"};
+            string[] g2Students = { "Hari", "Mata", "Hari" };
+
+            Console.WriteLine("Press `1` to see students in G1 or press `2` to see students in G2");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    for (int i = 0; i < g1Students.Length; i++)
+                    {
+                        Console.WriteLine(g1Students[i]);
+                    }
+                    break;
+                    case "2":
+                    for (int i = 0; i < g2Students.Length; i++)
+                    {
+                        Console.WriteLine(g2Students[i]);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error Wrong input, please restart app and try again because im layz and did bother making a while(true) ");
+                    break;
+            }
+        }
+    }
+}
