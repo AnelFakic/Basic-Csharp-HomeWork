75e05087aeadc10789b9bb31cfd1f76fb2553c3d
Anel Fakic <149324618+AnelFakic@users.noreply.github.com>
2024-03-06 15:50:25 +0100

    Add Homework #3

---
+++ Class 3 Homework/Class 3 Homework/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq.Expressions;
+
+namespace Class_3_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int number1;
+            int number2;
+            string mathOpp;
+
+            while (true)
+            {
+                Console.WriteLine("Please Input First number");
+                string inputA = Console.ReadLine();
+                 if(int.TryParse(inputA,out number1))
+                {
+                    break;
+                }
+                Console.WriteLine("Wrong first input try again");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Please Input Second number");
+                string inputB = Console.ReadLine();
+                if (int.TryParse(inputB, out number2))
+                {
+                    break;
+                }
+                Console.WriteLine("Wrong Secong input try again");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Please choose the mathOppematical operation you want to do buy choosing:`*`, `/, `+`, `-` ");
+               mathOpp= Console.ReadLine();
+                if ((mathOpp =="+")||(mathOpp=="-")|| (mathOpp == "*") || (mathOpp == "/"))
+                {
+                    break;
+                }
+                Console.WriteLine("Wrong mathOpp input try again buy choosing:`*`, `/, `+`, `-` \"");
+            }
+
+            switch (mathOpp)
+            {
+                case "+":
+                    Console.WriteLine($"{number1}+{number2}={number1 + number2}");
+                    break; 
+                case "-": Console.WriteLine($"{number1}-{number2}={number1 - number2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{number1}*{number2}={number1 * number2}" );
+                    break;
+                    case "/":
+                    Console.WriteLine($"{number1}/{number2}={number1 / number2}");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Class 3 Homework/Task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input First Number");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Input Second Number");
+            string input2 = Console.ReadLine();
+
+            int.TryParse(input1, out int CountUp);
+            int.TryParse(input2, out int CountDown);
+
+            Console.WriteLine("Counting Up");
+            for (int i = 1; i <= CountUp; i++)
+            {
+                
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Counting Down");
+            for (int i = 0;i < CountDown; CountDown--)
+            {
+               
+                Console.WriteLine(CountDown);
+            }
+
+        }
+    }
+}
