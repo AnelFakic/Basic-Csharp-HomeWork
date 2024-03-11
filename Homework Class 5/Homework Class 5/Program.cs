68d54c2ff8724c9953777da73c2ec524e9b0f258
Anel Fakic <149324618+AnelFakic@users.noreply.github.com>
2024-03-11 15:23:03 +0100

    Added Homework Class 5

---
+++ Homework Class 5/Homework Class 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Homework_Class_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture=new CultureInfo("en-US");
+
+            Console.WriteLine("Enter Birthday following this format: mm-dd-yyyy");
+            string birthDay =Console.ReadLine();
+            DateTime converted=DateTime.Parse(birthDay);
+
+            int age = AgeCalculator(converted);
+            Console.WriteLine($"Your Age is {age}");
+        }
+
+        public static int AgeCalculator(DateTime converted)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year -converted.Year;
+            
+            if (converted.Date >today.AddYears(-age))
+            {
+                age--;
+                
+            }
+
+            return age;
+        }
+
+    }
+}
---
+++ Homework Class 5/Task 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Hello from SEDC CODE 2024";
+            Console.WriteLine($"Hi ur phrase is : {phrase} lets modify it");
+
+            Substrings(phrase);
+
+
+        }
+
+        static void Substrings(string phrase)
+        {
+            Console.WriteLine("Enter a number for \"n\" to select strating index: ");
+            string inputN;
+            int n;
+            int m;
+            string substring = "";
+            string finalSubstring = "";
+            while (true){
+                inputN = Console.ReadLine();
+                if (!int.TryParse(inputN, out n))
+                {
+                    Console.WriteLine("Invalid N input try again");
+                    continue;
+                }
+                if (n > phrase.Length)
+                {
+                    Console.WriteLine("\"n\" cannot be larger than the length of our phrase try a smaller number:");
+                    continue;
+                }
+                substring = phrase.Substring(n);
+                break;
+            }
+
+            Console.WriteLine("Now enter the \"m\" for the max length of the phrase");
+            Console.WriteLine($"Make sur \"m\" is not larger than {substring.Length} ");
+           
+
+            while (true) {
+                
+                string inputM = Console.ReadLine();
+                if (!int.TryParse(inputM, out m)) {
+                    Console.WriteLine("Wrong M input try again");
+                    continue;
+                }
+                if (m > phrase.Length)
+                {
+                    Console.WriteLine("\"m\" cannot be larger than the length of our phrase try a smaller number:");
+                    continue;
+                }
+
+                    finalSubstring = substring.Substring(0, m);
+
+                Console.WriteLine($"Your new phrase is: {finalSubstring}");
+                Console.WriteLine($"Length of the new string is {finalSubstring.Length}");
+                break;
+            }
+        }
+    }
+}
