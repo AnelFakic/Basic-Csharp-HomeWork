a670a6f3b09063ee69aee667a91eda882534afbd
Anel Fakic <149324618+AnelFakic@users.noreply.github.com>
2024-03-16 00:48:26 +0100

    Added Homerwork 6

---
+++ Homework Class 6/Homework Class 6/Dog.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Homework_Class_6
+{
+    public class Dog
+    {
+        public string DogName { get; set; }
+        public string DogBreed { get; set; }
+        public string DogColor { get; set; }
+
+        public Dog(string dogName,string breed,string dogColor )
+        {
+            DogName = dogName;
+            DogBreed= breed;
+            DogColor = dogColor;
+            
+        }
+        public string DogCommand() 
+        {
+             string selectedCommand ="placeHolder";
+            while (true)
+            {
+                 string command = Console.ReadLine();
+                if (command=="1" || command== "2" || command=="3")
+                {
+                    switch (command)
+                    {
+                        case "1":
+                            selectedCommand="The dog is now eating";
+                            
+                         break;
+                        case "2":
+                            selectedCommand= "The dog is now playing";
+                            
+                            break;
+                        case "3":
+                            selectedCommand="The dog is chasing its tail";
+                            
+                         break;
+                    }
+                    Console.WriteLine(selectedCommand);
+                    return selectedCommand;
+                }
+                
+                else { 
+                Console.WriteLine("Wrong Input Try Again");
+                continue;
+                }
+
+            }
+        }
+
+
+    }
+}
---
+++ Homework Class 6/Homework Class 6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Channels;
+
+namespace Homework_Class_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName;
+            string dogBreed;
+            string dogColor;
+            string selectedCommand;
+
+            Console.WriteLine("Hello,Please enter infromation about your Dog");
+            Console.WriteLine("Name:");
+            dogName = Console.ReadLine();
+            Console.WriteLine("Breed:");
+            dogBreed = Console.ReadLine();
+            Console.WriteLine("Color:");
+            dogColor = Console.ReadLine();
+
+            Dog urDog = new Dog(dogName,dogBreed,dogColor);
+
+            Console.WriteLine($"Dog Name: {urDog.DogName} \n Dog Breed: {urDog.DogBreed} \n Dog Color: {urDog.DogColor}");
+
+            Console.WriteLine("Give ur dog a command, If u want him to eat press \"1\" " +
+                "\n If you want him to play press \"2\"  \n If you want him to run prees \"3\"");
+
+
+            urDog.DogCommand();
+           
+
+
+
+        }
+    }
+}
